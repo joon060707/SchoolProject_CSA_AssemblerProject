c05ccf15a4947969ad1b03f9b99d5e8316f35034
AI05 <joon060707@naver.com>
2021-10-21 19:00:15 +0900

    CPU

--- src/BC_Assembler.java
+++ src/BC_Assembler.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class BC_Assembler extends CPU {
 
 
@@ -5,7 +8,7 @@ public class BC_Assembler extends CPU {
 
 
     /*
-    * 코드 입력 팀
+    * 코드 입력(어셈블러) 팀
     * 1) Assembly.txt 파일을 읽어오기
     * 2) 읽어온 어셈블리어를 16진수로 변환 (First Pass, Second Pass)
     * 3) memory[4096]에 16진수 형태로 저장
@@ -13,6 +16,91 @@ public class BC_Assembler extends CPU {
     *
     *
     * */
+
+    private static void runAssembler(String file) {
+        int cnt=0;
+        int lc;
+        String[] temp1 = new String[4096];
+        String[][] temp2 = new String[4096][4];
+        // 라벨필드 | 명령어 | 주소 | I
+
+        try {
+            // 버퍼리더에 파일 등록
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            // 파일을 temp1에 저장, 달성 후 cnt = 명령어+1
+            while(true) {
+                String line = br.readLine();
+                if(line==null) break;
+                temp1[cnt++] = line;
+            }
+            br.close();
+
+
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+            System.out.println("<temp1>");
+            for(int i=0;i<9;i++) {
+                System.out.println(temp1[i]);
+            }
+            System.out.println();
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+            // temp1을 temp2로 이전
+            for(int i=0;i<temp1.length;i++) {
+                if(temp1[i] != null) {
+                    String[] temp3 = temp1[i].split(" ");
+                    for(int j=0;j<temp3.length;j++) {
+                        temp2[i][j]=temp3[j];
+                    }
+                }
+            }
+
+            // 라벨 주소 위치 맞혀주기
+            for(int i=0;i<temp2.length;i++) {
+                if(temp1[i] != null) {
+                    if(!(temp2[i][0].contains(","))) {
+                        temp2[i][3]=temp2[i][2];
+                        temp2[i][2]=temp2[i][1];
+                        temp2[i][1]=temp2[i][0];
+                        temp2[i][0]=null;
+                    }
+                }
+            }
+
+
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+            System.out.println("<temp2>");
+            for(int i=0;i<9;i++) {
+                for(int j=0;j<4;j++) {
+                    System.out.print(temp2[i][j]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+            // ORG 값대로 location counter 초기화
+            lc=Integer.parseInt(temp2[0][2]);
+
+
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+            System.out.println("<location counter>");
+            System.out.println(lc+"\n");
+            //ㅡㅡㅡㅡㅡㅡㅡㅡㅡTEST용 출력ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+
+            for(int i=1;i<memory.length;i++);
+            // memory[i] = (short)temp1[i];
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+        tempMemory();
+    }
 
     /*
      * Fetch & Decode 팀
@@ -23,6 +111,25 @@ public class BC_Assembler extends CPU {
      *
      *
      * */
+    static void tempMemory(){
+        memory[0] = 0x2004;
+        memory[1] = 0x1005;
+        memory[2] = 0x3006;
+        memory[3] = 0x7001;
+        memory[4] = 0x0053;
+        memory[5] = (short) 0xFFE9;
+        memory[6] = 0x0000;
+    }
+
+    static void fetch(){
+        reg_AR = reg_PC;
+    }
+    static void decode(){
+        reg_IR = memory[reg_AR];
+        reg_PC++;
+        ff_I = reg_IR<0;
+        reg_AR = (short) (reg_IR & 0x0fff);
+    }
 
     /*
      * Instruction Set 팀
@@ -40,32 +147,28 @@ public class BC_Assembler extends CPU {
      *
      *
      * */
+    static void execute(){
+        System.out.println("IR\t\tAR\tPC\tDR\t\tAC\t\tTR");
+        System.out.println(String.format("%04x\t%03x\t%03x\t%04x\t%04x\t%04x", reg_IR, reg_AR, reg_PC, reg_DR, reg_AC, reg_TR));
+    }
 
 
 
     public static void main(String[] args) {
 
         // main 함수의 시작은 코드 입력팀에서 작성하는 함수로 시작.
+        runAssembler("src/Assembly.txt");
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        while (true){
 
+             fetch();
+             decode();
+             execute();
 
-
-
-
-
-
-
-
-
-
-
-
-
+             if(reg_IR == 0x7001) {
+                System.out.println("HALT COMPUTER");
+                break;
+             }
+        }
     }
 }
