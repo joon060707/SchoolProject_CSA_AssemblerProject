3d077314bf6859c95edfaeddbfd32cfe57076fd8
AI05 <joon060707@naver.com>
2021-10-22 11:15:19 +0900

    프로젝트 진행 방향

--- src/BC_Assembler.java
+++ src/BC_Assembler.java
@@ -101,6 +101,33 @@ public class BC_Assembler extends CPU {
 
         tempMemory();
     }
+    static void tempMemory(){
+        memory[0] = 0x2004;
+        memory[1] = (short) 0x1005;
+        memory[2] = 0x3006;
+        memory[3] = 0x7001;
+        memory[4] = (short) 0x0053;
+        memory[5] = (short) 0xFFE9;
+        memory[6] = 0x0000;
+    }
+
+    static void printInstruction(){
+        int instructionSize = 7;        // 이 개수는 Second Pass에서 LC나 다른 변수에서 갖고 와야 함
+        int org = 0;                    // 이 개수는 어셈블러에서 첫 번째 org 옆 숫자 값이어야 함
+
+        System.out.println("---저장된 기계어입니다---");
+        for(int i=org; i<instructionSize; i++){
+            System.out.printf("M[%04x] = %04x\n", i, memory[i]);
+        }
+        System.out.println("---기계어의 끝입니다---");
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
     /*
      * Fetch & Decode 팀
@@ -111,25 +138,61 @@ public class BC_Assembler extends CPU {
      *
      *
      * */
-    static void tempMemory(){
-        memory[0] = 0x2004;
-        memory[1] = 0x1005;
-        memory[2] = 0x3006;
-        memory[3] = 0x7001;
-        memory[4] = 0x0053;
-        memory[5] = (short) 0xFFE9;
-        memory[6] = 0x0000;
-    }
+
 
     static void fetch(){
+        // T0
         reg_AR = reg_PC;
-    }
-    static void decode(){
+        reg_SC++;
+        // T1
         reg_IR = memory[reg_AR];
         reg_PC++;
+        reg_SC++;
+        System.out.printf("--- Memory[%03x]: %04x 실행 ---\n", reg_AR, reg_IR);
+    }
+
+    // decode 함수에서 해석된 임시 디코드 문자열
+    static String decodedInstruction;
+
+    static void decode(){
+        // T2
         ff_I = reg_IR<0;
         reg_AR = (short) (reg_IR & 0x0fff);
+        reg_SC++;
+
+        // T3
+        // Iooo aaaa aaaa aaaa -> IIII Iooo
+        byte opc = (byte)(reg_IR >>> 12);
+//        System.out.println(opc);
+
+        if(opc == -1 || opc == 7){      // 1111 1111(=-1)이거나 0000 0111(=7)
+            switch (reg_IR){
+                case 0x7001: decodedInstruction = "HLT"; break;
+                default:  decodedInstruction = "ERR";
+            }
+        } else {
+
+            if(ff_I) reg_AR = (short) (memory[reg_AR] & 0x0fff);
+            reg_SC++;
+
+            switch (opc){
+                case 0, -8: decodedInstruction = "AND"; break;
+                case 1, -7: decodedInstruction = "ADD"; break;
+                case 2, -6: decodedInstruction = "LDA"; break;
+                case 3, -5: decodedInstruction = "STA"; break;
+                case 4, -4: decodedInstruction = "BUN"; break;
+                case 5, -3: decodedInstruction = "BSA"; break;
+                case 6, -2: decodedInstruction = "ISZ"; break;
+                default:  decodedInstruction = "ERR";
+            }
+        }
+
     }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
+
+
 
     /*
      * Instruction Set 팀
@@ -140,6 +203,45 @@ public class BC_Assembler extends CPU {
      *
      * */
 
+    static void LDA(){
+        // T4
+        reg_DR = memory[reg_AR];
+        reg_SC++;
+        // T5
+        reg_AC = reg_DR;
+    }
+
+    static void ADD(){
+        // T4
+        reg_DR = memory[reg_AR];
+        reg_SC++;
+        // T5
+        // 올바르게 계산되기 위해 사용되지 않는 부분을 클리어
+        int temp = (reg_AC & 0x0000ffff) + (reg_DR & 0x0000ffff);
+//        System.out.println(Integer.toBinaryString(temp));
+        ff_E = temp >>> 16 == 1;  // 둘 다 음수인 경우 E에 저장됨. (오버플로우)
+        reg_AC += reg_DR;
+    }
+
+    static void STA(){
+        // T4
+        memory[reg_AR] = reg_AC;
+    }
+
+
+    static void HLT(){
+        ff_S = false;
+    }
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
     /*
      * Execution 팀
      * 1) Fetch & Decode 팀의 출력값에 따라 적절한 명령어 실행하도록 코딩
@@ -148,27 +250,73 @@ public class BC_Assembler extends CPU {
      *
      * */
     static void execute(){
-        System.out.println("IR\t\tAR\tPC\tDR\t\tAC\t\tTR");
-        System.out.println(String.format("%04x\t%03x\t%03x\t%04x\t%04x\t%04x", reg_IR, reg_AR, reg_PC, reg_DR, reg_AC, reg_TR));
+
+        System.out.println("해석 결과: "+(ff_I?"(간접) ":"")+decodedInstruction);
+        switch (decodedInstruction){
+            case "LDA": LDA(); break;
+            case "ADD": ADD(); break;
+            case "STA": STA(); break;
+            case "HLT": HLT(); break;
+            default: break;
+        }
+        // 모든 명령이 끝나면 공통으로 SC=0이 됨.
+        reg_SC = 0;
+        System.out.print("IR\t\tAR\tPC\tDR\t\tAC\t\tTR\t\t");
+        System.out.println("I\tS\tE");
+        System.out.print(String.format("%04X\t%03X\t%03X\t%04X\t%04X\t%04X\t", reg_IR, reg_AR, reg_PC, reg_DR, reg_AC, reg_TR));
+        System.out.println(String.format("%X\t%X\t%X", ff_I?1:0, ff_S?1:0, ff_E?1:0));
     }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////////////
 
 
 
     public static void main(String[] args) {
 
-        // main 함수의 시작은 코드 입력팀에서 작성하는 함수로 시작.
+        // 메인 함수는 어셈블러 실행 - {fetch - decode - execute}로만 구성. 나머지 작업은 다른 곳에서.
         runAssembler("src/Assembly.txt");
+        printInstruction();
 
-        while (true){
+        System.out.println("--- 명령어 실행 시작 ---");
+        System.out.print("IR\t\tAR\tPC\tDR\t\tAC\t\tTR\t\t");
+        System.out.println("I\tS\tE");
+        System.out.print(String.format("%04X\t%03X\t%03X\t%04X\t%04X\t%04X\t", reg_IR, reg_AR, reg_PC, reg_DR, reg_AC, reg_TR));
+        System.out.println(String.format("%X\t%X\t%X", ff_I?1:0, ff_S?1:0, ff_E?1:0));
 
+
+        while (ff_S){       // start-stop flip-flop이 1일 때만 작동.
              fetch();
              decode();
              execute();
-
-             if(reg_IR == 0x7001) {
-                System.out.println("HALT COMPUTER");
-                break;
-             }
         }
+        System.out.println("--- 명령어 실행 끝 ---");
+        System.out.println("--- 컴퓨터를 종료합니다. ---");
+
+
+
+//        short a = (short)0xffff;
+//        short b = (short)0x8fef;
+//        int c = a + b;
+//        int d = (short) c;
+//        int e = (c << 15);
+//        int f = (c << 15) >>> 31;
+//        System.out.println(Integer.toBinaryString(a));
+//        System.out.println(Integer.toBinaryString(b));
+//        System.out.println(Integer.toBinaryString(c));
+//        System.out.println(Integer.toBinaryString(d));
+//        System.out.println(Integer.toBinaryString(e));
+//        System.out.println(Integer.toBinaryString(f));
+//
+//        int g = 0b1111111100000000;
+//        int h = g >>> 12;
+//        System.out.println(Integer.toBinaryString(h));
+//        System.out.println(h);
+
+
     }
+
+
+
 }
